5d4b2dc2fb926f40fc176f19e01380224555838c
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-05-15 10:07:44 +0100

    writing the school registry contract

--- contracts/SchoolRegistry.sol
+++ contracts/SchoolRegistry.sol
@@ -2,8 +2,27 @@
 
 pragma solidity ^0.8.26;
 
-contract schoolRegistry{
+contract SchoolRegistry{
     // State variables
+    string public schoolName;
+    address public admin;
+    address public teacherContract;
+    address public studentContract;
 
+    // Mapping
+    mapping(address => bool) public isTeacher;
+    mapping(address => bool) public isStudent;
     
+    // Events
+    event TeacherRegistered(address indexed teacherWallet, uint256 subjectId);
+    
+    // Modifiers
+    modifier onlyAdmin(){
+        require(msg.sender == admin, "Not admin");
+        _;
+    }
+
+    constructor(address _teacherContract, address _studentContract, string memory _schoolName){
+        require(_teacherContract != address(0), "Invalid teacher contract");
+    }
 }
