2fdf9dfb8576e5b8c9703dba9c99b9f9ce4932d4
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-05-15 05:14:12 +0100

    first commit

---
+++ contract/StudentContract.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.26;
+
+contract teacherContract{
+
+    // State Variables
+
+    // Struct
+    struct Teacher{
+        uint256 teacherId,
+        string teacherName
+        
+    }
+
+    mapping ()
+}
