64a88f043b434ef5e5d5430d5e58c3f207712bb5
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-05-18 00:46:47 +0100

    Student contract

---
+++ contracts/SchoolToken.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract schoolToken {
+    string public name = "School Token";
+    string public symbol = "SCT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    
+}
