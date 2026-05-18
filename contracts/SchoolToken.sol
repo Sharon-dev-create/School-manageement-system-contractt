805136e51949dd604f5cb1583f76669ca6e9392c
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-05-18 01:06:28 +0100

    Student contract

--- contracts/SchoolToken.sol
+++ contracts/SchoolToken.sol
@@ -8,5 +8,18 @@ contract schoolToken {
     uint8 public decimals = 18;
     uint256 public totalSupply;
 
-    
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Mint(address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+
+    constructor(uint256 _initialSupply){
+        totalSupply = _initialSupply * (10 ** uint256(decimals));
+
+        balanceOf[msg.sender] = totalSupply;
+
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
 }
